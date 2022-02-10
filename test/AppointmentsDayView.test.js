3820e11a212c570922b6a0d5b582a8be07667fc4
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-10 00:40:43 -0300

    test(AppoinmentsDayView): add "is each item a li" case

--- test/AppointmentsDayView.test.js
+++ test/AppointmentsDayView.test.js
@@ -33,4 +33,20 @@ describe('AppointmentsDayView', () => {
     expect(container.querySelector('ol')).not.toBeNull();
     expect(container.querySelector('ol').children).toHaveLength(2);
   });
+
+  it('renders each appointment in an li', () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) },
+    ];
+    const component = <AppointmentsDayView appointments={appointments} />;
+    const container = createContainer();
+
+    render(container)(component);
+
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+    expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+  });
 });
