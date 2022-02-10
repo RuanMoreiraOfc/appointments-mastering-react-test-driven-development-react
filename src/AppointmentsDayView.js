07c6f839d935ac165873c3e6de2af5126736cb61
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-10 10:42:10 -0300

    feat(AppoinmentsDayView): make button change selected appointment

--- src/AppointmentsDayView.js
+++ src/AppointmentsDayView.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Appointment } from './Appointment';
 
 export { AppointmentsDayView };
 
-const AppointmentsDayView = ({ appointments }) => (
-  <div id='appointmentsDayView'>
-    <ol>
-      {appointments.map((appointment) => (
-        <li key={appointment.startsAt}>
-          <button type='button'>
-            {dateToTimeString(appointment.startsAt)}
-          </button>
-        </li>
-      ))}
-    </ol>
-    {appointments.length === 0 ? (
-      <p>There are no appointments scheduled for today.</p>
-    ) : (
-      <Appointment customer={appointments[0].customer} />
-    )}
-  </div>
-);
+const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
+  return (
+    <div id='appointmentsDayView'>
+      <ol>
+        {appointments.map((appointment, i) => (
+          <li key={appointment.startsAt}>
+            <button
+              type='button'
+              onClick={setSelectedAppointment.bind(null, i)}
+            >
+              {dateToTimeString(appointment.startsAt)}
+            </button>
+          </li>
+        ))}
+      </ol>
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment customer={appointments[selectedAppointment].customer} />
+      )}
+    </div>
+  );
+};
 
 const dateToTimeString = (time) => {
   const date = new Date(time);
