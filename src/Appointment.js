b297d0d8bdac4f27da873297e6de255cc8d4a7e3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-10 18:31:52 -0300

    feat(Appoinment): pass lastName param

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,5 +3,7 @@ import React from 'react';
 export { Appointment };
 
 const Appointment = ({ customer: { firstName, lastName } }) => (
-  <div>{firstName} Garcia</div>
+  <div>
+    {firstName} {lastName}
+  </div>
 );
