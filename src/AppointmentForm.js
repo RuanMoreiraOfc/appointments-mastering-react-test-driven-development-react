0b130cb67f2b70a248bb0cd7d9339df1b0cd5054
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-12 20:26:44 -0300

    feat(AppointmentForm): pass selectableServices param

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,9 +1,23 @@
 export { AppointmentForm };
 
-const AppointmentForm = () => (
+const AppointmentForm = ({ selectableServices }) => (
   <form id='appointment'>
     <select name='service'>
       <option />
+      {selectableServices.map((service) => (
+        <option key={service}>{service}</option>
+      ))}
     </select>
   </form>
 );
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cut',
+    'Blow-dry',
+    'Cut & color',
+    'Beard trim',
+    'Cut & beard trim',
+    'Extensions',
+  ],
+};
