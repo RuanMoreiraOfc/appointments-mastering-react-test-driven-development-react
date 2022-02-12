e63f2690f17b679ca7d5f0c4b43f831c30d7638c
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-11 22:21:30 -0300

    test(CustomerForm): add 'has first name text field' case

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -15,4 +15,17 @@ describe('CustomerForm', () => {
     const form = getFormFrom(container)('customer');
     expect(form).not.toBeNull();
   });
+
+  it('renders the first name field as a text box', () => {
+    const component = <CustomerForm />;
+    const { container, render } = createContainer();
+
+    render(component);
+
+    const form = getFormFrom(container)('customer');
+    const field = form.elements.firstName;
+    expect(field).toBeTruthy();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
+  });
 });
