0d1fbf206a778eae71c1d6f5da07bb7d5d8c17c1
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-11 22:11:28 -0300

    refactor(CustomerForm): create `getFromFrom` function

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -3,12 +3,16 @@ import { createContainer } from './utils/domManipulators';
 import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
+  const getFormFrom = (container) => (id) =>
+    container.querySelector(`form[id="${id}"]`);
+
   it('renders a form', () => {
     const component = <CustomerForm />;
     const { container, render } = createContainer();
 
     render(component);
 
-    expect(container.querySelector('form[id="customer"]')).not.toBeNull();
+    const form = getFormFrom(container)('customer');
+    expect(form).not.toBeNull();
   });
 });
