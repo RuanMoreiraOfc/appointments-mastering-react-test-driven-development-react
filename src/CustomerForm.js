6cafd4626356649c6c39b5b0131787955f89b959
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-11 23:00:06 -0300

    feat(CustomerForm): pass onSubmit param

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,13 +1,17 @@
 export { CustomerForm };
 
-const CustomerForm = ({ firstName }) => (
-  <form id='customer'>
-    <label htmlFor='firstName'>First name</label>
-    <input
-      id='firstName'
-      type='text'
-      name='firstName'
-      defaultValue={firstName}
-    />
-  </form>
-);
+const CustomerForm = ({ firstName, onSubmit }) => {
+  const customer = { firstName };
+
+  return (
+    <form id='customer' onSubmit={() => onSubmit(customer)}>
+      <label htmlFor='firstName'>First name</label>
+      <input
+        id='firstName'
+        type='text'
+        name='firstName'
+        defaultValue={firstName}
+      />
+    </form>
+  );
+};
