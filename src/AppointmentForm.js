fbdd2bd0eda1e9d069feb373c78ac05dc43c9eb1
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-02-13 21:53:41 -0300

    feat(AppointmentForm): renders time intervals as th

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -3,9 +3,11 @@ import { useState } from 'react';
 export { AppointmentForm };
 
 const AppointmentForm = ({
-  selectableServices, //
+  selectableServices,
   service,
   onSubmit,
+  salonOpensAt,
+  salonClosesAt,
 }) => {
   const [appointment, setAppointment] = useState({ service });
 
@@ -25,7 +27,10 @@ const AppointmentForm = ({
           <option key={service}>{service}</option>
         ))}
       </select>
-      <TimeSlotTable />
+      <TimeSlotTable
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+      />
     </form>
   );
 };
@@ -39,6 +44,50 @@ AppointmentForm.defaultProps = {
     'Cut & beard trim',
     'Extensions',
   ],
+  salonOpensAt: 9,
+  salonClosesAt: 19,
 };
 
-const TimeSlotTable = () => <table id='time-slots' />;
+const TimeSlotTable = ({ salonOpensAt, salonClosesAt }) => {
+  const timeSlots = createTimeSlots({
+    intervalInMinutes: 30,
+    salonOpensAt,
+    salonClosesAt,
+  });
+
+  return (
+    <table id='time-slots'>
+      <tbody>
+        {timeSlots.map((timeAsString) => (
+          <tr key={timeAsString}>
+            <th>{timeAsString}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const createTimeSlots = ({
+  intervalInMinutes,
+  salonOpensAt,
+  salonClosesAt,
+}) => {
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+
+  const intervalCountBySlot = 60 / intervalInMinutes;
+  const intervalInMilliseconds = (60 * 60 * 1000) / intervalCountBySlot;
+  const totalSlotsCount = (salonClosesAt - salonOpensAt) * intervalCountBySlot;
+
+  const result = Array.from({ length: totalSlotsCount }, (_, i) => {
+    const time = startTime + intervalInMilliseconds * i;
+    const timeAsString = new Date(time).toLocaleTimeString(['en-UK'], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+
+    return timeAsString;
+  });
+
+  return result;
+};
