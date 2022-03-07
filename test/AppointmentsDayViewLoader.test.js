abb200c62835851f1a73eede90b694d590a33d36
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-07 12:17:53 -0300

    test(AppointmentsDayViewLoader): add 're-fetches when prop changed' case

--- test/AppointmentsDayViewLoader.test.js
+++ test/AppointmentsDayViewLoader.test.js
@@ -68,4 +68,22 @@ describe('AppointmentsDayViewLoader', () => {
       AppointmentsDayViewExports.AppointmentsDayView,
     ).toHaveBeenLastCalledWith({ appointments }, expect.anything());
   });
+
+  it('re-requests appointment when today prop changes', async () => {
+    const tomorrow = new Date(today);
+    tomorrow.setHours(24);
+    const from = tomorrow.setHours(0, 0, 0, 0);
+    const to = tomorrow.setHours(23, 59, 59, 999);
+    const component = <AppointmentsDayViewLoader today={today} />;
+    const componentTomorrow = <AppointmentsDayViewLoader today={tomorrow} />;
+    const { renderAndPromise } = createContainer();
+
+    await renderAndPromise(component);
+    await renderAndPromise(componentTomorrow);
+
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      `/appointments/${from}-${to}`,
+      expect.anything(),
+    );
+  });
 });
