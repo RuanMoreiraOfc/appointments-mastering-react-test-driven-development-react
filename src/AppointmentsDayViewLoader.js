bcee2b50b876f3a8bdf47746d3cf3137342ca0c3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-07 10:45:52 -0300

    feat(AppointmentsDayViewLoader): save fetched data in state

--- src/AppointmentsDayViewLoader.js
+++ src/AppointmentsDayViewLoader.js
@@ -1,26 +1,30 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { AppointmentsDayView } from './AppointmentsDayView';
 
 export { AppointmentsDayViewLoader };
 
 const AppointmentsDayViewLoader = ({ today }) => {
+  const [appointments, setAppointments] = useState([]);
+
   useEffect(() => {
     const from = today.setHours(0, 0, 0, 0);
     const to = today.setHours(23, 59, 59, 999);
 
-    const fetchAppointments = () => {
-      window.fetch(`/appointments/${from}-${to}`, {
+    const fetchAppointments = async () => {
+      const response = await window.fetch(`/appointments/${from}-${to}`, {
         method: 'GET',
         credentials: 'same-origin',
         headers: { 'Content-Type': 'application/json' },
       });
+
+      setAppointments(await response.json());
     };
 
     fetchAppointments();
   }, []);
 
-  return <AppointmentsDayView appointments={[]} />;
+  return <AppointmentsDayView appointments={appointments} />;
 };
 
 AppointmentsDayViewLoader.defaultProps = {
