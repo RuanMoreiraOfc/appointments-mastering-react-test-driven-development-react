a4477c8ec4c15699470e250d7f7c8d36fda6f897
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-23 23:45:59 -0300

    test(App): add 'renders a button' case

--- test/App.test.js
+++ test/App.test.js
@@ -1,4 +1,9 @@
-import { createShallowRenderer, compareByType } from './utils/shallowHelpers';
+import {
+  createShallowRenderer,
+  getChildrenFrom,
+  compareByType,
+  compareByClassName,
+} from './utils/shallowHelpers';
 
 import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader';
 import { App } from '../src/App';
@@ -25,5 +30,18 @@ describe('App', () => {
     const element = getChild(0);
     expect(element.type).toEqual('div');
     expect(element.props.className).toEqual('button-bar');
+  });
+
+  it('has a button to initiate add customer and appointment action', () => {
+    const component = <App />;
+    const { render, getElementsMatching } = createShallowRenderer();
+
+    render(component);
+
+    const buttons = getChildrenFrom(
+      getElementsMatching(compareByClassName('button-bar'))[0],
+    );
+    expect(buttons[0].type).toEqual('button');
+    expect(buttons[0].props.children).toEqual('Add customer and appointment');
   });
 });
